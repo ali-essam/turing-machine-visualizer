0e82aff362234573f4bfca74fb70a68f01ea9a07
Ali Essam <ali.essam.arafa@gmail.com>
2016-05-07 01:54:18 +0200

    Tape Visulaiztion

---
+++ arr-vis.js
@@ -0,0 +1,36 @@
+var $ = require('jquery');
+var _ = require('underscore');
+
+function ArrayVisualizer(container, arr, initalVal) {
+  this.arr = (typeof initalVal != undefined)?_.map(_.range(arr), function () { return initalVal; }):arr;
+  this.container = $(container);
+  this.rootElem = null;
+  this.onClick = null;
+
+  var __construct = function(self) {
+    self.container.append('<ul id="tape" class="pagination"></ul>');
+    self.rootElem = self.container.children().first();
+    self.arr.forEach(function(x) {
+      self.rootElem.append('<li><a href="#">' + x +'</a></li>')
+    });
+  }(this)
+}
+
+ArrayVisualizer.prototype.selectElementAt = function (index) {
+  this.clearSelection()
+  this.rootElem.children().eq(index).addClass('active');
+};
+
+ArrayVisualizer.prototype.clearSelection = function () {
+  this.rootElem.children().removeClass('active');
+};
+
+ArrayVisualizer.prototype.updateArray = function (newArr) {
+  this.rootElem.children().each(function(i, elem) {
+    $(elem).children().first().text(newArr[i]);
+  })
+};
+
+module.exports = {
+  'ArrayVisualizer': ArrayVisualizer
+}
--- index.js
+++ index.js
@@ -2,32 +2,87 @@ var $ = require("jquery");
 var _ = require("underscore");
 var tl = require("turing-lang");
 var vis = require("vis");
+var arrvis = require("./arr-vis");
 
 var visdata = {
     nodes: new vis.DataSet(),
     edges: new vis.DataSet()
 };
 var network = new vis.Network($('#mynetwork')[0], visdata,
-  {layout:{hierarchical:{direction: 'LR'}}});
+  {
+    layout: {hierarchical:{direction: 'LR'}},
+    edges: {labelHighlightBold: false}
+  });
 var machine = tl.parse('');
 
-$('#btnRun').click( function () {
+var TAPE_LEFT = -10;
+var TAPE_Right = 10;
+
+tp = new arrvis.ArrayVisualizer($('#test'), 20, 0);
+
+
+function getSortedTape(machine, from, to) {
+  var mp = machine.tape.readBulk(from,to);
+  var keys = _.sortBy(_.keys(mp), function (key) {
+    return key;
+  });
+  return _.map(keys, function(k) { return mp[k] + "" });
+}
+
+$('#btnParse').click(function () {
   machine = tl.parse(getTxtTransText());
   console.log(machine);
-  transitionTable = machine.transitionFunction.transitionTable;
 
-  stateNames = _.uniq(_.flatten(_.map(_.values(transitionTable), function(val1) {
+  setGraphVis(getMachineGraph(machine), visdata);
+
+  enableControls();
+
+  machine.on('step', function(inf) {
+    network.selectNodes([machine.currentState], false);
+    var arr = getSortedTape(machine, TAPE_LEFT, TAPE_Right);
+    tp.updateArray(arr);
+    tp.selectElementAt(inf.headPosition + TAPE_LEFT);
+  });
+
+  machine.on('error', function(e) {
+    console.error(e);
+  });
+
+});
+
+$('#btnRun').click(function() {
+  machine.run(1500);
+});
+
+$('#btnStep').click(function() {
+  machine.step();
+});
+
+$('#btnFinish').click(function() {
+  machine.run(0);
+});
+
+function setGraphVis(graph, visdata) {
+  visdata.nodes.clear();
+  visdata.nodes.add(graph.nodes);
+
+  visdata.edges.clear();
+  visdata.edges.add(graph.edges);
+}
+
+function getMachineGraph(machine) {
+  var transitionTable = machine.transitionFunction.transitionTable;
+
+  var stateNames = _.uniq(_.flatten(_.map(_.values(transitionTable), function(val1) {
     return _.map(_.values(val1), function(val2) {
       return val2.state;
     });
   })));
-  newNodes = _.map(stateNames, function(val) {
-    return _.object([['id', val], ['label', val]])
+  var nodes = _.map(stateNames, function(val) {
+    return _.object([['id', val], ['label', val]]);
   });
-  visdata.nodes.clear();
-  visdata.nodes.add(newNodes);
 
-  newEdges = _.flatten(_.map(transitionTable, function(toStates, from) {
+  var edges = _.flatten(_.map(transitionTable, function(toStates, from) {
     return _.map(toStates, function(val, key) {
       return _.object([
         ['from', from],
@@ -37,18 +92,35 @@ $('#btnRun').click( function () {
       ]);
     })
   }), true);
-  visdata.edges.clear();
-  visdata.edges.add(newEdges);
-  machine.on('step', function() {
-    console.log(machine.currentState);
-  });
-  machine.run(1000);
+
+  return {
+    nodes: nodes,
+    edges: edges
+  }
+}
+
+var transOldVal = "";
+$("#txtTrans").on("change keyup paste", function() {
+    var currentVal = $(this).val();
+    if(currentVal == transOldVal) {
+        return; //check to prevent multiple simultaneous triggers
+    }
+
+    transOldVal = currentVal;
+    //action to be performed on textarea changed
+    disableControls();
 });
 
 function getTxtTransText() {
    return $('#txtTrans').val();
 }
-//network.setData(data);
-// console.log(network);
-// console.log(data);
-// nodes.update({id:2, color:'red'});
+
+function disableControls() {
+  $('#btnParse').removeClass('disabled');
+  $('.btnCtrl').addClass('disabled');
+}
+
+function enableControls() {
+  $('#btnParse').addClass('disabled');
+  $('.btnCtrl').removeClass('disabled');
+}
